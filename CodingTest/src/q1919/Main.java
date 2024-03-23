71c8dd7377d87b067439597f3bb1642295ec65e5
JHK <rootboot00@gmail.com>
2024-03-23 23:15:18 +0900

    q1919, q2744 내용 업데이트

--- CodingTest/src/q1919/Main.java
+++ CodingTest/src/q1919/Main.java
@@ -2,40 +2,61 @@ package q1919;
 
 import java.util.Scanner;
 
+//	BOJ 1919번 문제
+//	https://www.acmicpc.net/problem/1919
+
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		String a = sc.next();
 		String b = sc.next();
-		
-		// 카운트 배열 !
-		int[] countA = getAlphabetCount(a);
-		int[] countB = getAlphabetCount(b);
+		answer01(a, b);
+		answer02(a, b);
+	}
 
-		int ans = 0;
+	//	1. 카운트 배열을 만들어, 각 문자열에 어떤 알파벳이 몇개있는지 찾아 두 문자열의 차이 계산하기
+	public static void answer01(String a, String b) {
+		int[] countA = new int[26];
+		int[] countB = new int[26];
 		
-		for(int i=0; i<26; i++)
-		ans += Math.abs(countA[i] - countB[i]);
-/*		{
+		for(int i=0; i<a.length(); i++) {
+			countA[a.charAt(i) - 'a']++;
+		}
+		for(int i=0; i<a.length(); i++) {
+			countB[b.charAt(i) - 'a']++;
+		}
+		
+		int answer = 0;
+		for(int i=0; i<26; i++) {
 			if(countA[i] > countB[i]) {
-				ans += countA[i] - countB[i];
+				answer += countA[i] - countB[i];
 			}else if(countB[i] > countA[i]) {
-				ans += countB[i] - countA[i];
+				answer += countB[i] - countA[i];
 			}
 		}
-*/
-		System.out.println(ans);
+		System.out.println(answer);
 	}
 	
+	//	2. 카운트 배열 만드는 메서드를 만들어 재사용하고, 두 문자열의 차이는 절대값을 이용하기
 	public static int[] getAlphabetCount(String str) {
 		int[] count = new int[26];
-		
 		for(int i=0; i<str.length(); i++) {
 			count[str.charAt(i) - 'a']++;
 		}
-		
 		return count;
 	}
+	
+	public static void answer02(String a, String b) {
+		int[] countA = getAlphabetCount(a);
+		int[] countB = getAlphabetCount(b);
+		
+		int answer = 0;
+		for(int i=0; i<26; i++) {
+			answer += Math.abs(countA[i] - countB[i]);
+		}
+		System.out.println(answer);
+	}
+	
+	
 }
--- CodingTest/src/q2744/Main.java
+++ CodingTest/src/q2744/Main.java
@@ -2,17 +2,68 @@ package q2744;
 
 import java.util.Scanner;
 
+//	BOJ 2744번 문제
+//	https://www.acmicpc.net/problem/2744
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String inputString = sc.nextLine();
-		for(int i=0; i<inputString.length(); i++) {
-			if('A'<=inputString.charAt(i) && inputString.charAt(i)<='Z') {
-				System.out.print((char)('a' + inputString.charAt(i) - 'A'));
-			}else {
-				System.out.print((char)('A' + inputString.charAt(i) - 'a'));
+		String inputMessage = sc.next();
+		answer01(inputMessage);
+		System.out.println();
+		answer02(inputMessage);
+		answer03(inputMessage);
+	}
+	
+	//	ASCII Table을 이용해, 특정 문자가 어떤 숫자(십진수)로 표현되는지를 이용한다.
+	//	(예 : 'A' = 65, 'Z' = 90 ...)
+	//	('Z' = 'A') = 25
+	
+	public static void answer01(String str) {		
+		//	1. 반복문을 돌려 대소문자 판별하기
+		for(int i=0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			//	2. 대문자면 'A'와의 수차이를 계산해서 'a'에 더하기
+			if(ch >= 'A' && ch <= 'Z') {
+				System.out.print((char)('a' + ch - 'A'));
+			}
+			else {
+				//	3. 소문자면 'a'와의 수차이를 계산해서 'A'에 더하기
+				System.out.print((char)('A' + ch - 'a'));
 			}
 		}
 	}
+	
+	//	빈 문자열을 만들어 하나씩 추가하기
+	//	단, 계속해서 새로운 인스턴스를 만드는 방식이라. 비효율적일 수 있음.
+	public static void answer02(String str) {
+		String answer = "";
+		for(int i=0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch >= 'A' && ch <= 'Z') {
+				answer += (char)('a' + ch - 'A');
+			}
+			else {
+				answer += (char)('A' + ch - 'a');
+			}
+		}
+		System.out.println(answer);
+	}
+	
+	// 문자열을 문자 배열로 바꿔 처리하기 "WrongAnswer" => ['W', 'o', 'r', 'n', ...]
+	public static void answer03(String str) {
+		char[] answer = str.toCharArray();
+		for(int i=0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch >= 'A' && ch <= 'Z') {
+				answer[i] = (char)('a' + ch - 'A');
+			}
+			else {
+				answer[i] = (char)('A' + ch - 'a');
+			}
+		}
+		System.out.println(answer);
+	}
+	
 }
 
